e809555b81a505c838b46efd045c133fda964d13
Lech Głowiak <LGLO@users.noreply.github.com>
2019-09-22 15:15:29 +0200

    Update to RC13, remove core-tests dependency, update other dependencies (#33)

--- interop-scalaz7x/jvm/src/test/scala/zio/interop/scalazPlatform72Spec.scala
+++ interop-scalaz7x/jvm/src/test/scala/zio/interop/scalazPlatform72Spec.scala
@@ -1,17 +1,15 @@
 package zio
 package interop
 
-import org.scalacheck.{ Arbitrary, Cogen }
-import org.specs2.ScalaCheck
+import org.scalacheck.{ Arbitrary, Cogen, _ }
+import org.specs2.{ ScalaCheck, Specification }
 import scalaz.Scalaz._
 import scalaz.scalacheck.ScalazProperties._
-import zio.interop.scalaz72._
 import scalaz._
+import zio.interop.scalaz72._
+import zio.internal.PlatformLive
 
-class scalazPlatform72Spec(implicit ee: org.specs2.concurrent.ExecutionEnv)
-    extends TestRuntime
-    with ScalaCheck
-    with GenIO {
+class scalazPlatform72Spec extends Specification with ScalaCheck with GenIO {
 
   def is = s2"""
     laws must hold for
@@ -23,11 +21,18 @@ class scalazPlatform72Spec(implicit ee: org.specs2.concurrent.ExecutionEnv)
       MonadError             ${monadError.laws[IO[Int, ?], Int]}
       Applicative (Parallel) ${applicative.laws[scalaz72.ParIO[Any, Int, ?]]}
   """
+  type Env = Any
+
+  private val rts: Runtime[Env] = new DefaultRuntime {
+    override val Platform = PlatformLive
+      .makeDefault()
+      .withReportFailure(_ => ())
+  }
 
   implicit def ioEqual[E: Equal, A: Equal]: Equal[IO[E, A]] =
     new Equal[IO[E, A]] {
       override def equal(io1: IO[E, A], io2: IO[E, A]): Boolean =
-        unsafeRun(io1.either) === unsafeRun(io2.either)
+        rts.unsafeRun(io1.either) === rts.unsafeRun(io2.either)
     }
 
   implicit def ioArbitrary[E: Arbitrary: Cogen, A: Arbitrary: Cogen]: Arbitrary[IO[E, A]] =
@@ -38,4 +43,46 @@ class scalazPlatform72Spec(implicit ee: org.specs2.concurrent.ExecutionEnv)
 
   implicit def ioParArbitrary[E: Arbitrary: Cogen, A: Arbitrary: Cogen]: Arbitrary[scalaz72.ParIO[Any, E, A]] =
     Arbitrary(genIO[E, A].map(Tag.apply))
+}
+
+trait GenIO {
+
+  /**
+   * Given a generator for `A`, produces a generator for `IO[E, A]` using the `IO.point` constructor.
+   */
+  def genSyncSuccess[E, A: Arbitrary]: Gen[IO[E, A]] = Arbitrary.arbitrary[A].map(IO.succeed[A](_))
+
+  /**
+   * Given a generator for `A`, produces a generator for `IO[E, A]` using the `IO.async` constructor.
+   */
+  def genAsyncSuccess[E, A: Arbitrary]: Gen[IO[E, A]] =
+    Arbitrary.arbitrary[A].map(a => IO.effectAsync[E, A](k => k(IO.succeed(a))))
+
+  /**
+   * Randomly uses either `genSyncSuccess` or `genAsyncSuccess` with equal probability.
+   */
+  def genSuccess[E, A: Arbitrary]: Gen[IO[E, A]] = Gen.oneOf(genSyncSuccess[E, A], genAsyncSuccess[E, A])
+
+  /**
+   * Given a generator for `E`, produces a generator for `IO[E, A]` using the `IO.fail` constructor.
+   */
+  def genSyncFailure[E: Arbitrary, A]: Gen[IO[E, A]] = Arbitrary.arbitrary[E].map(IO.fail[E])
+
+  /**
+   * Given a generator for `E`, produces a generator for `IO[E, A]` using the `IO.async` constructor.
+   */
+  def genAsyncFailure[E: Arbitrary, A]: Gen[IO[E, A]] =
+    Arbitrary.arbitrary[E].map(err => IO.effectAsync[E, A](k => k(IO.fail(err))))
+
+  /**
+   * Randomly uses either `genSyncFailure` or `genAsyncFailure` with equal probability.
+   */
+  def genFailure[E: Arbitrary, A]: Gen[IO[E, A]] = Gen.oneOf(genSyncFailure[E, A], genAsyncFailure[E, A])
+
+  /**
+   * Randomly uses either `genSuccess` or `genFailure` with equal probability.
+   */
+  def genIO[E: Arbitrary, A: Arbitrary]: Gen[IO[E, A]] =
+    Gen.oneOf(genSuccess[E, A], genFailure[E, A])
+
 }
